e25dd05a59631215d7cb4342cea85f2cbc86ef43
Robert Cassin III <73715966+rcassin3@users.noreply.github.com>
2023-06-25 21:50:38 -0400

    Update Lab7.R

--- Example1/Lab7.R
+++ Example1/Lab7.R
@@ -20,8 +20,8 @@ ggplot(data = groupAvg, aes(Species, y = meanGroup, fill = Treatment)) +
   geom_bar(stat = "identity", position = "dodge") +
   theme_minimal() +
   scale_fill_manual(values = c('red', 'blue', 'yellow', 'grey')) +
-  theme(text = element_text(size = 27), plot.title = element_text(hjust = 0.5)) +
-  geom_errorbar(aes(ymin = meanGroup - sdGroup, ymax = meanGroup + sdGroup), position = "dodge", linewidth = 1.5) +
+  theme(text = element_text(size = 19), plot.title = element_text(hjust = 0.5)) +
+  geom_errorbar(aes(ymin = meanGroup - sdGroup, ymax = meanGroup + sdGroup), position = "dodge") +
   labs(title = "Bacterial Growth after Treatment" , y = "Bacteria Proxy (Abs)")
 
 res.aov2 <- aov(Abs ~ Treatment + Species, data = groupWorkDF)
@@ -31,42 +31,3 @@ res.aov3 <- aov(Abs ~ Treatment * Species, data = groupWorkDF)
 summary(res.aov3)
 
 TukeyHSD(res.aov3)
-
-
-
-
-
-
-
-
-
-
-#|||||||||||||||||||||||| SECOND DATA SET ||||||||||||||||||||||||||||||||||
-
-soloDF <- read.csv('organizedData.csv')
-
-soloAvg <- soloDF %>% group_by(Species, Treatment) %>% 
-  summarise(mean = mean(Abs), sd = sd(Abs))
-
-soloAvg$Species <- factor(soloAvg$Species, 
-                           levels = c('bkg', 'A. hyd', 'B. sub', 'C.fre',
-                                      'E.col', 'P.flu', 'S. epi', 'S. mar'))
-
-
-windows()
-ggplot(data = soloAvg, aes(Species, y = mean, fill = Treatment)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  theme_light() +
-  scale_fill_manual(values = c('red', 'blue', 'yellow', 'grey')) +
-  theme(text = element_text(size = 19), plot.title = element_text(hjust = 0.5)) +
-  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), position = "dodge") +
-  labs(title = "Bacterial Growth after Treatment" , y = "Bacteria Proxy (Abs)")
-
-res.aov22 <- aov(Abs ~ Treatment + Species, data = soloDF)
-summary(res.aov22)
-
-res.aov32 <- aov(Abs ~ Treatment * Species, data = soloDF)
-summary(res.aov32)
-
-TukeyHSD(res.aov32)
-
